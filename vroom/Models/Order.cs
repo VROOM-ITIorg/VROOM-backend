96a46204bc0636a346c350c8ad806bb0327b3c0a
Rowyda020 <rowydaramadan@gmail.com>
2025-03-16 23:30:08 +0200

    Order Table

---
+++ vroom/Models/Order.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace vroom.Models
+{
+    public class Order
+    {
+        public int Id { get; set; } 
+        public string Details { get; set; }
+        public OrderState State { get; set; }
+        public decimal  OrderPrice { get; set; }
+        public decimal DeliveryPrice { get; set; }
+        public DateTime Date { get; set; }
+        public string Title { get; set; }
+        public float Weight { get; set; }
+        public string? Notes { get; set; }
+        public bool IsBreakable { get; set; }
+
+        public ICollection<OrderPriority>? Priority { get; set; } 
+
+
+        public int RiderId { get; set; } //fk to the rider
+
+        public int BusinessId { get; set; } //fk to the Business Owner
+
+        public int PaymentId { get; set; } //fk to the Payment
+
+
+    }
+    public class OrderConfig: IEntityTypeConfiguration<Order>
+    {
+        public void Configure(EntityTypeBuilder<Order> builder)
+        {
+            builder.HasKey(o=>o.Id);
+            builder.Property(o => o.Title).HasMaxLength(100).IsRequired();
+            builder.Property(o=>o.Date).IsRequired();
+            builder.Property(e => e.Weight).HasColumnType("decimal(18,2)").IsRequired();
+            builder.Property(o => o.Notes).HasMaxLength(100);
+            builder.Property(o => o.IsBreakable).IsRequired();
+            builder.Property(e => e.Details).HasMaxLength(1000);
+            //builder.Property(e => e.State).HasDefaultValue(0);
+            builder.Property(e => e.OrderPrice).HasColumnType("decimal(18,2)").IsRequired();
+
+            builder.Property(e => e.DeliveryPrice).HasColumnType("decimal(18,2)").IsRequired();
+
+        }
+    }
+}
---
+++ vroom/orderContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using vroom.Models;
+
+namespace vroom
+{
+    public class OrderContext : DbContext
+    {
+        public DbSet<Order> orders;
+        public DbSet<OrderPriority> priority;
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data source = ROZIE\\MSSQLSERVER01 ; Initial catalog = Vroom; Integrated security= true; trustservercertificate = true;");
+            base.OnConfiguring(optionsBuilder);
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new OrderConfig());
+            modelBuilder.ApplyConfiguration(new OrderPriorityConfig());
+  
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}

756aae116944e4a25c16035ea7ebc2d645acdc08
AhmedShams999 <106814036+AhmedShams999@users.noreply.github.com>
2025-03-25 19:41:29 +0200

    Add shipment repo

--- VROOM.API/obj/Debug/net8.0/VROOM.API.AssemblyInfo.cs
+++ VROOM.API/obj/Debug/net8.0/VROOM.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VROOM.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64ca9aba26ca8a77b21bdbd7f752431114c8a43a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e2d265a38ff63efb662cc459e60d17dbbcc8a97")]
 [assembly: System.Reflection.AssemblyProductAttribute("VROOM.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VROOM.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- VROOM.Models/Shipment/Shipment.cs
+++ VROOM.Models/Shipment/Shipment.cs
@@ -12,13 +12,23 @@ namespace VROOM.Models
     {
         public int Id{ get; set; }
         public int RiderID { get; set; }
-        public string Beginning { get; set; }
-        public string End { get; set; }
+        public DateTime Beginning { get; set; }
+        public DateTime End { get; set; }
+        public ShipmentStatus Status { get; set; }
         public int MaxConsecutiveDeliveries { get; set; }
         public virtual Rider Rider { get; set; }
         public virtual Route Route { get; set; }
         public bool IsDeleted { get; set; } = false;
         public string? ModifiedBy { get; set; }
         public DateTime? ModifiedAt { get; set; } = DateTime.Now;
+    }
+
+    public enum ShipmentStatus
+    {
+        Pending,
+        InTransit,
+        Delivered,
+        Cancelled,
+        Delayed
     }
 }

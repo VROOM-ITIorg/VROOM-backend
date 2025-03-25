7f7a6549b212723e367fec4262f6c1dd4093ae25
Rowyda020 <rowydaramadan@gmail.com>
2025-03-25 18:36:59 +0200

    Added AssignRiderToBusinessOwner functionality

--- VROOM.Repositories/BusinessOwnerRepo.cs
+++ VROOM.Repositories/BusinessOwnerRepo.cs
@@ -6,75 +6,96 @@ using System.Threading.Tasks;
 using VROOM.Data;
 using VROOM.Models;
 
+/*
+0- CRUD 
+1- Get Business Owners by Business Type
+2- Get Riders for a Business Owner
+3- Assign a Rider to a Business Owner 
+4- Remove a Rider from a Business Owner
+5- Pagination //not yet
+6- edge cases //not yet
+ 
+ */
+
 
 namespace VROOM.Repositories
 {
     
         public class BusinessOwnerManager
         {
-            private readonly MyDbContext _dbContext;
-            public BusinessOwnerManager(MyDbContext dbContext)
-            {
+           private readonly MyDbContext _dbContext;
+           public BusinessOwnerManager(MyDbContext dbContext)
+           {
                 _dbContext = dbContext;
-            }
+           }
 
 
-            public List<BusinessOwner> GetAllBusinessOwners()
-            {
-                return _dbContext.BusinessOwners.ToList();
-            }
+        public List<BusinessOwner> GetAllBusinessOwners()
+        {
+              return _dbContext.BusinessOwners.ToList();
+        }
 
 
-            public BusinessOwner GetById(int id)
-            {
-                return _dbContext.BusinessOwners.FirstOrDefault(b => b.Id == id);
-            }
+        public BusinessOwner GetById(int id)
+        {
+             return _dbContext.BusinessOwners.FirstOrDefault(b => b.Id == id);
+        }
 
 
-            public void AddBusinessOwner(BusinessOwner businessOwner)
-            {
-                _dbContext.BusinessOwners.Add(businessOwner);
-                _dbContext.SaveChanges();
-            }
+        public void AddBusinessOwner(BusinessOwner businessOwner)
+        {
+              _dbContext.BusinessOwners.Add(businessOwner);
+              _dbContext.SaveChanges();
+        }
 
 
-            public int UpdateBusinessOwner(BusinessOwner businessOwner)
-            {
-                _dbContext.BusinessOwners.Update(businessOwner);
-                return _dbContext.SaveChanges();
-            }
+        public int UpdateBusinessOwner(BusinessOwner businessOwner)
+        {
+              _dbContext.BusinessOwners.Update(businessOwner);
+              return _dbContext.SaveChanges();
+        }
 
 
-            public void DeleteBusinessOwner(int id)
-            {
-                var selectedBusinessOwner = _dbContext.BusinessOwners.FirstOrDefault(b => b.Id == id);
-                if (selectedBusinessOwner != null)
-                {
+        public void DeleteBusinessOwner(int id)
+        {
+             var selectedBusinessOwner = _dbContext.BusinessOwners.FirstOrDefault(b => b.Id == id);
+             if (selectedBusinessOwner != null)
+             {
                     _dbContext.BusinessOwners.Remove(selectedBusinessOwner);
                     _dbContext.SaveChanges();
-                }
-            }
+             }
+        }
 
 
-            public List<BusinessOwner> GetBusinessOwnersByType(string businessType)
-            {
+        public List<BusinessOwner> GetBusinessOwnersByType(string businessType)
+        {
                 return _dbContext.BusinessOwners
                     .Where(b => b.BusinessType == businessType)
                     .ToList();
-            }
+        }
 
 
-            public List<Rider> GetRidersForBusinessOwner(int businessOwnerId)
-            {
+        public List<Rider> GetRidersForBusinessOwner(int businessOwnerId)
+        {
                 return _dbContext.Riders
                     .Where(r => r.BusinessID == businessOwnerId)
                     .ToList();
-            }
+        }
+
+        public void AssignRiderToBusinessOwner(int businessOwnerId, int riderId)
+        {
+            _dbContext.RiderAssignments.Add(new RiderAssignment
+            {
+                BusinessID = businessOwnerId,
+                RiderID = riderId,
+                AssignmentDate = DateTime.UtcNow
+            });
+
+            _dbContext.SaveChanges();
+        }
 
 
-
-
-            public void RemoveRiderFromBusinessOwner(int riderId)
+        public void RemoveRiderFromBusinessOwner(int riderId)
             {
                 var rider = _dbContext.Riders.FirstOrDefault(r => r.Id == riderId);
                 if (rider != null)
@@ -85,10 +106,6 @@ namespace VROOM.Repositories
             }
 
 
-
-
-
-
-        }
     }
+}
 

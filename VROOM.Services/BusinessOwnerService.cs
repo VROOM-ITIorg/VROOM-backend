b28c20d8ada21c81ff9bac14e6da6b11da1dced2
Ibrahim_Mamdouh <hemamamdouh76@gmail.com>
2025-06-19 17:19:33 +0300

    Update assign auto

--- VROOM.Services/BusinessOwnerService.cs
+++ VROOM.Services/BusinessOwnerService.cs
@@ -826,6 +826,7 @@ namespace VROOM.Services
                     });
 
                     shipment.RiderID = riderId;
+                    shipment.ShipmentState = ShipmentStateEnum.Assigned;
                     shipmentRepository.Update(shipment);
                     shipmentRepository.CustomSaveChanges();
 
@@ -1253,7 +1254,7 @@ namespace VROOM.Services
 
                         if (distance > threshold)
                         {
-                            Waypoint waypoint = new Waypoint() { ShipmentID = shipment.Id, Lang = shipment.EndLang, Lat = shipment.EndLat, Area = shipment.EndArea };
+                            Waypoint waypoint = new Waypoint() { ShipmentID = shipment.Id, Lang = shipment.EndLang, Lat = shipment.EndLat, Area = shipment.EndArea, orderId = order.Id };
                             shipment.waypoints.Add(waypoint);
                             shipment.EndLat = newLat;
                             shipment.EndLang = newLng;
@@ -1261,7 +1262,7 @@ namespace VROOM.Services
                         }
                         else
                         {
-                            Waypoint waypoint = new Waypoint() { ShipmentID = shipment.Id, Lang = route.DestinationLang, Lat = route.DestinationLat, Area = route.DestinationArea };
+                            Waypoint waypoint = new Waypoint() { ShipmentID = shipment.Id, Lang = route.DestinationLang, Lat = route.DestinationLat, Area = route.DestinationArea, orderId = order.Id };
                             shipment.waypoints.Add(waypoint);
                         }
 
